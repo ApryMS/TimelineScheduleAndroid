8cf3d158aadef546871430038509cc0973824fab
Apry Madona Sihaloho <madonaapry@gmail.com>
2022-08-24 11:58:40 +0700

    functional finish next clear cache image

--- app/src/main/java/deploy/com/timelineschedule/ui/home/detailtimeline/TimelineDetailResponse.kt
+++ app/src/main/java/deploy/com/timelineschedule/ui/home/detailtimeline/TimelineDetailResponse.kt
@@ -5,9 +5,6 @@ import deploy.com.timelineschedule.ui.DataUser
 
 data class TimelineDetailResponse(
 
-	@field:SerializedName("task")
-	val task: List<TaskItem>,
-
 	@field:SerializedName("timeline")
 	val timeline: Timeline,
 
@@ -47,36 +44,6 @@ data class Timeline(
 
 	@field:SerializedName("image")
 	val image: String,
-
-	@field:SerializedName("updatedAt")
-	val updatedAt: String
-)
-
-data class TaskItem(
-
-	@field:SerializedName("createdAt")
-	val createdAt: String,
-
-	@field:SerializedName("inviteBy")
-	val inviteBy: String,
-
-	@field:SerializedName("__v")
-	val V: Int,
-
-	@field:SerializedName("name")
-	val name: String,
-
-	@field:SerializedName("timeline")
-	val timeline: String,
-
-	@field:SerializedName("_id")
-	val id: String,
-
-	@field:SerializedName("worker")
-	val worker: DataUser,
-
-	@field:SerializedName("status")
-	val status: String,
 
 	@field:SerializedName("updatedAt")
 	val updatedAt: String
